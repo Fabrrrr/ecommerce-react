a9f9663b4712f3b425b3e80ee2d759a9ef7e15cf
fabrrrr <wonerless@gmail.com>
2024-10-01 16:18:29 -0300

    test

--- src/db/firebase.utils.js
+++ src/db/firebase.utils.js
@@ -14,10 +14,17 @@ import {
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
 
 // Initialize Firebase
-const firebaseApp = initializeApp(process.env.REACT_APP_FIREBASECONFIG);
+const firebaseApp = initializeApp(firebaseConfig);
 
 // Create google auth and make a mandatory account selection
 const googleProvider = new GoogleAuthProvider();
